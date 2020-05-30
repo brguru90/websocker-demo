0ac9f94593de28c0f4473eb7323cb57538494112
Guruprasad BR <guruprasad.br@terralogic.com>
2020-05-30 22:46:53 +0530

    production server setup

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+var compression = require("compression")
 const app = express()
 // const restify = require("restify");
 // const app = restify.createServer()
@@ -11,13 +12,23 @@ app.use(function (req, res, next) {
     return next()
 })
 
+// eslint-disable-next-line no-undef
+app.use(compression())
+app.use(
+    express.json({
+        limit: "50mb",
+        extended: false,
+    })
+)
+// eslint-disable-next-line no-undef
+app.use("/", express.static(__dirname + "/../test_app/build"))
 app.use("/api/", require("./api/test"))
 app.use("/ws/", require("./ws/chat"))
 
-app.get("/", function (req, res) {
-    console.log("get route")
-    res.send("hi")
-})
+// app.get("/", function (req, res) {
+//     console.log("get route")
+//     res.send("hi")
+// })
 
 // app.ws('/', function (ws, req) {
 //     ws.on('message', function (msg) {
@@ -26,6 +37,10 @@ app.get("/", function (req, res) {
 //     console.log('socket', req.testing);
 // });
 
+// eslint-disable-next-line no-undef
+console.log(`DocumentRoot ${__dirname + "/../test_app/build"}`)
+
 app.listen(port, () => {
-    console.log(`App started running at ${port}`)
+    // eslint-disable-next-line no-undef
+    console.log(`App started running at ${port}\ncwd is ${__dirname}`)
 })
